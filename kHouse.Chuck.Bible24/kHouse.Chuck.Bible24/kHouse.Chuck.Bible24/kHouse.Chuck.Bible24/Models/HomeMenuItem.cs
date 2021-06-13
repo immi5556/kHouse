d70927bbfae183f2d3a484b521103aa8abdff684
jiraj <immanuelraj.j@clariant.com>
2021-06-13 11:35:00 +0530

    changes

---
+++ kHouse.Chuck.Bible24/kHouse.Chuck.Bible24/kHouse.Chuck.Bible24/kHouse.Chuck.Bible24/Models/HomeMenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kHouse.Chuck.Bible24.Models
+{
+    public enum MenuItemType
+    {
+        Browse,
+        About
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ kHouse.Chuck.Bible24/kHouse.Chuck.Bible24/kHouse.Chuck.Bible24/kHouse.Chuck.Bible24/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using kHouse.Chuck.Bible24.Models;
+
+namespace kHouse.Chuck.Bible24.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
